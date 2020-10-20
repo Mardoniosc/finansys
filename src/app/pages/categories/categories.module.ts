053c58225f9682de4fd5c58e4155f9a0230025f7
programadorsemcortes <programadorsemcortes@gmail.com>
2020-10-20 15:37:11 -0300

    Cloncluindo a implementaçao do CategoryList

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
@@ -8,6 +7,6 @@ import { CategoryFormComponent } from './category-form/category-form.component';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
-  imports: [CommonModule, CategoriesRoutingModule, HttpClientModule],
+  imports: [CommonModule, CategoriesRoutingModule],
 })
 export class CategoriesModule {}
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService, Category } from '../shared';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      (data) => (this.categories = data),
+      (err) => console.error('Erro ao carregar Lista ', err)
+    );
   }
 
+  deleteCategory(category: Category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        (data) => {
+          this.categories = this.categories.filter((x) => x != category);
+        },
+        (err) => alert('Erro ao tentar excluir!!')
+      );
+    }
+  }
 }
