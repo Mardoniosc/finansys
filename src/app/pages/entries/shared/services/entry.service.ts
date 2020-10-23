ad5ae64d30879f1f3bceaeb5e113206e464deb55
Mardoniosc <mardonio@live.com>
2020-10-23 14:35:37 -0300

    Melhorias no 'EntryService' (reduzindo mais repetições)

--- src/app/pages/entries/shared/services/entry.service.ts
+++ src/app/pages/entries/shared/services/entry.service.ts
@@ -4,7 +4,7 @@ import { BaseResourceService } from 'src/app/shared/services/base-resource.servi
 import { Entry } from '../models';
 import { CategoryService } from 'src/app/pages/categories/shared';
 import { Observable } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -18,20 +18,23 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap((category) => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    );
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  private setCategoryAndSendToServer(
+    entry: Entry,
+    sendFn: any
+  ): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap((category) => {
         entry.category = category;
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     );
   }
 }
