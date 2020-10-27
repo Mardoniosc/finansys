380e2fd28c402675a3538e3a0eecba6189374050
Mardoniosc <mardonio@live.com>
2020-10-27 11:11:07 -0300

    Utilizando o componente 'BreadCrumb'

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components';
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [BreadCrumbComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [
     // Módulos
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
 
     // Components
     BreadCrumbComponent,
