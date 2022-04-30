e6bcc7071868ee3179f78179ae59a4724b22b49b
Mardonio Costa <mardonio.costa@tecnisys.com.br>
2022-04-30 17:08:39 -0300

    Criando componentes e servico de usuário

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserFormComponent, UserListComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
