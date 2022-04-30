f92824705efb90f76fc7346e2b49390cb7a87013
Mardonio Costa <mardonio.costa@tecnisys.com.br>
2022-04-30 17:11:18 -0300

    Criando componentes e servico de perfils

--- src/app/pages/profiles/profiles.module.ts
+++ src/app/pages/profiles/profiles.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfilesRoutingModule } from './profiles-routing.module';
+import { ProfileListComponent } from './profile-list/profile-list.component';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfileListComponent, ProfileFormComponent],
   imports: [
     CommonModule,
     ProfilesRoutingModule
