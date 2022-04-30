712baf9508567a90ff60c80e329b4a89987a8c6c
Mardonio Costa <mardonio.costa@tecnisys.com.br>
2022-04-30 17:00:51 -0300

    Criando modulos adicionais

---
+++ src/app/pages/permitions/permitions.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PermitionsRoutingModule } from './permitions-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PermitionsRoutingModule
+  ]
+})
+export class PermitionsModule { }
